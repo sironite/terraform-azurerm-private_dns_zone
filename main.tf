e9b5f330724f1d1ccede022fa6a20c481ebbc83e
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-06-29 14:57:09 +0200

    fix: variables

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ resource "azurerm_private_dns_zone" "this" {
   dynamic "soa_record" {
     for_each = var.soa_record_enabled ? [1] : []
     content {
-      email         = var.soa_record_email
-      host          = var.soa_record_host
-      minimum_ttl   = var.soa_record_minimum_ttl
-      refresh_time  = var.soa_record_refresh_time
-      retry_time    = var.soa_record_retry_time
-      serial_number = var.soa_record_serial_number
-      ttl           = var.soa_record_ttl
+      email        = var.soa_record_email
+      expire_time  = var.soa_record_expire_time
+      minimum_ttl  = var.soa_record_minimum_ttl
+      refresh_time = var.soa_record_refresh_time
+      retry_time   = var.soa_record_retry_time
+      ttl          = var.soa_record_ttl
+      tags         = var.soa_record_tags
     }
   }
 
