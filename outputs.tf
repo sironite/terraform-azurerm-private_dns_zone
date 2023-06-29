ead1062ece8cf0141b24da178dbd6ff4b5e96fad
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-06-29 14:49:00 +0200

    Style: Docs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "private_dns_zone_id" {
   description = "The ID of the private DNS zone."
-  value = azurerm_private_dns_zone.this.id
+  value       = azurerm_private_dns_zone.this.id
 }
 
 output "private_dns_zone_name" {
   description = "The name of the private DNS zone."
-  value = azurerm_private_dns_zone.this.name
+  value       = azurerm_private_dns_zone.this.name
 }
